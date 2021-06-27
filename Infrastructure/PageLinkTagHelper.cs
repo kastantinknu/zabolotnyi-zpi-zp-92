1c8f6f076cb0c934d71978f60253a0bdb96a0d82
kastantinknu <48106683+kastantinknu@users.noreply.github.com>
2021-06-27 11:30:40 +0300

    navbar

--- Infrastructure/PageLinkTagHelper.cs
+++ Infrastructure/PageLinkTagHelper.cs
@@ -24,6 +24,11 @@ namespace Football_manager.Infrastructure
         public ViewContext ViewContext { get; set; }
         public Paginginfo PageModel { get; set; }
         public string PageAction { get; set; }
+
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext context,
         TagHelperOutput output)
         {
@@ -34,6 +39,12 @@ namespace Football_manager.Infrastructure
                 TagBuilder tag = new TagBuilder("a");
                 tag.Attributes["href "] = urlHelper.Action(PageAction,
                 new { PlayerPage = i });
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                    ? PageClassSelected : PageClassNormal);
+                }
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
