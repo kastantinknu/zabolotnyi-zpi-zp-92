971af0e3e2654c46a293d63d1f991b4871e500c0
kastantinknu <48106683+kastantinknu@users.noreply.github.com>
2021-06-27 16:54:56 +0300

    category backmenu

--- Components/NavigationMenuViewComponent.cs
+++ Components/NavigationMenuViewComponent.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Football_manager.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Football_manager.Components
 {
     public class NavigationMenuViewComponent : ViewComponent
     {
-        public string Invoke()
+        private IPlayerRepository repository;
+        public NavigationMenuViewComponent(IPlayerRepository repo)
         {
-            return "Hello from the Nav View Component";
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Players
+            .Select(x => x.TeamId)
+            .Distinct()
+            .OrderBy(x => x));
         }
     }
 }
