5e5119438ba97a71c9cb90e87d7d63f05f004ea4
kastantinknu <48106683+kastantinknu@users.noreply.github.com>
2021-06-29 21:15:25 +0300

    edit docer script

--- Docker.ps1
+++ Docker.ps1
@@ -1,10 +1,10 @@
-﻿#heroku login
+﻿heroku login
 docker ps -a
 docker build -t counter-image-six-two -f Dockerfile .
 docker create --name core-counter-six-two counter-image-six-two
 docker ps -a
-docker tag counter-image-six-two registry.heroku.com/appstory-my/web
+docker tag counter-image-six-two registry.heroku.com/zabolotnyi-zpi-zp-92/web
 heroku container:login
-docker push registry.heroku.com/appstory-my/web
-heroku container:release web -a=appstory-my
-heroku logs --tail -a=appstory-my
+docker push registry.heroku.com/zabolotnyi-zpi-zp-92/web
+heroku container:release web -a=zabolotnyi-zpi-zp-92
+heroku logs --tail -a=zabolotnyi-zpi-zp-92
