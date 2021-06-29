e4fdcb3ab025dcf37c23b122193208bfc4ba08d2
kastantinknu <48106683+kastantinknu@users.noreply.github.com>
2021-06-29 17:43:52 +0300

    add admin layout, admin index and controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Football_manager.Models;
+using Microsoft.AspNetCore.Mvc;
+//using System.Linq;
+namespace Football_manager.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IPlayerRepository repository;
+        public AdminController(IPlayerRepository repo) {
+            repository = repo;
+        }
+        public ViewResult Index() => View(repository.Players);
+        //public ViewResult Edit(int Id) =>
+        //        View(repository.Players
+        //        .FirstOrDefault(p => p.Id == Id));
+    }
+}
